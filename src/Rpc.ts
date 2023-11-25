a8ae59e3a2480f05d043c7833595d9e92a9d0c4d
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-11-25 22:37:23 +0100

    more environment setups

--- src/Rpc.ts
+++ src/Rpc.ts
@@ -3,8 +3,17 @@ class Rpc {
     constructor(network: string) {
         this.network = network;
     }
+
+    get node() {
+        return "hello";
+    }
 }
 
 const demo: Rpc = new Rpc("PulseChain");
+const demo2: string = demo.node;
+const demo3: string = new Rpc("Bsc").node;
 
 console.log(demo);
+console.log(demo2);
+console.log(demo3);
+console.log(demo.network);
