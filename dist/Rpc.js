652f8de324c782cc1b3bfac8e5be21b891db6051
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-11-25 18:33:09 +0100

    removed dist

--- dist/Rpc.js
+++
@@ -1,9 +0,0 @@
-class Rpc {
-    constructor(network) {
-        this.network = network;
-    }
-}
-const demo = new Rpc("PulseChain");
-console.log(demo);
-export {};
-//# sourceMappingURL=Rpc.js.map
