5c04b19b0acdb078ebec412753647f4fc1830605
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-11-28 23:14:11 +0100

    added a test to test my safety mechanics

--- src/test/Rpc.test.ts
+++ src/test/Rpc.test.ts
@@ -22,5 +22,13 @@ describe('Rpc Class', () => {
         expect(eth.node).toBe('https://endpoints.omniatech.io/v1/eth/mainnet/public');
         expect(matic.node).toBe('https://polygon-bor.publicnode.com');
     });
+
+    test('should start from the beginning end of list is reached', () => {
+        const boba = new Rpc('boba');
+        expect(boba.node).toBe('https://mainnet.boba.network');
+        expect(boba.node).toBe('https://boba-ethereum.gateway.tenderly.co');
+        expect(boba.node).toBe('https://gateway.tenderly.co/public/boba-ethereum');
+        expect(boba.node).toBe('https://mainnet.boba.network');
+    });
 });
 
