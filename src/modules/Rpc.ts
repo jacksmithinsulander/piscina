aea7df50e87d6a18733113190fb5d3b7291f2465
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-11-28 21:48:35 +0100

    started writing out tests for indexing through rpcs

--- src/modules/Rpc.ts
+++ src/modules/Rpc.ts
@@ -1,11 +1,26 @@
+//import { rpcList } from '../data/rpcList';
+
+interface RpcList {
+    [network: string]: number;
+}
+
+
 class Rpc {
     public network: string;
+    private static list: RpcList = {};
+
     constructor(network: string) {
         this.network = network;
+
+        if (!Rpc.list[network]) {
+            Rpc.list[network] = 0;
+        }
     }
 
     get node() {
-        return "hello";
+        const networkList = Rpc.list[this.network];
+        Rpc.list[this.network] += 1;
+        return networkList;
     }
 }
 
--- src/test/Rpc.test.ts
+++ src/test/Rpc.test.ts
@@ -1,7 +1,26 @@
 import { Rpc } from '../modules/Rpc'
 
-const rpc: Rpc = new Rpc("PulseChain");
+describe('Rpc Class', () => {
+    let rpc: Rpc;
 
-test('should return an instance of Rpc', () => {
-    expect(rpc instanceof Rpc);
+    beforeEach(() => {
+        rpc = new Rpc('PulseChain');
+    });
+
+    test('should return an instance of Rpc', () => {
+        expect(rpc instanceof Rpc).toBe(true);
+    });
+
+    test('should return incremented indexes', () => {
+        expect(rpc.node).toBe(0);
+        expect(rpc.node).toBe(1);
+        expect(rpc.node).toBe(2);
+    });
+
+    test('should handle multiple instances with separate mappings', () => {
+        const anotherRpc = new Rpc('Ethereum');
+        expect(rpc.node).toBe(0);
+        expect(anotherRpc.node).toBe(0);
+    });
 });
+
