26505357ba4bfe82cfa04222a1d6d44b1b45d9cf
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-12-10 23:00:03 +0100

    removed some tests

--- src/modules/pair.abi.ts
+++
@@ -1,8 +0,0 @@
-import { Abi } from "@hazae41/cubane"
-
-export namespace PairAbi {
-  export const getReserves = {
-    input: Abi.createFunctionSignature("getReserves",Abi.createFunctionSelectorAndArguments(Abi.FunctionSelector.from([9,2,241,172]),Abi.createTuple())),
-    output: Abi.createTuple(Abi.Uint112, Abi.Uint112, Abi.Uint32)
-  } as const
-}
