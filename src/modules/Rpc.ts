56f48cd93955f18e0c16e951efa8e9b76dc635a7
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-12-10 11:56:40 +0100

    fixed docstrings

--- src/modules/Rpc.ts
+++ src/modules/Rpc.ts
@@ -1,14 +1,31 @@
 import rpcList from '../data/rpcList';
 
-
+/**
+ * Interface defining the index for RPCs by network.
+ */
 interface RpcIndex {
     [network: string]: number;
 }
 
+/**
+ * Class representing an RPC.
+ */
 class Rpc {
+    /**
+     * The network associated with the RPC.
+     */
     public network: string;
+
+    /**
+     * Index to keep track of RPCs by network.
+     */
     private rpcIndex: RpcIndex = {};
 
+    /**
+     * Constructs an RPC instance for a given network.
+     * @param {string} network - The network to which the RPC belongs.
+     * @throws {Error} Throws an error if the network is not present in the rpcList.
+     */
     constructor(network: string) {
         this.network = network;
 
@@ -23,7 +40,11 @@ class Rpc {
         }
     }
     
-    get node() {
+    /**
+     * Retrieves the next RPC node for the network.
+     * @returns {string} The RPC node URL.
+     */
+    get node(): string {
         const links: string[] = rpcList[this.network];
         const index: number = this.rpcIndex[this.network] % links.length;
         this.rpcIndex[this.network] += 1;
@@ -31,4 +52,5 @@ class Rpc {
     }
 }
 
-export {Rpc};
+export { Rpc };
+
