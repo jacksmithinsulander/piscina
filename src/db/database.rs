a1e79be7ed20c4efdb9b2506b730a5b32d49b1a2
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-01-20 11:58:19 +0100

    trying to get things to work with a database

---
+++ src/db/database.rs
@@ -0,0 +1,15 @@
+#[derive(Serialize, Deserialize)]
+struct LiquidityPool {
+    uid: i32,
+    chain: str,
+    time_of_creation: i32,
+    token_a_name: str,
+    token_a_symbol: str,
+    token_a_amount: i32,
+    token_a_price: i32,
+    token_b_name: str,
+    token_b_symbol: str,
+    token_b_amount: i32,
+    token_b_price: i32,
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 // use provider_factory::test_import;
 mod ethereum;
 mod tests;
-mod database;
 
 fn main() {
     println!("Hello World!!!");
-    database::db::main();
 }
